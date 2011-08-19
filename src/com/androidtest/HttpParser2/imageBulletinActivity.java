b77b67eb4d182698f76ff6cffffae3f33e3c0fd8
bogus532 <bogus532@gmail.com>
2011-08-19 16:36:08 +0900

    changed imagebulletinactivity

--- src/com/androidtest/HttpParser2/imageBulletinActivity.java
+++ src/com/androidtest/HttpParser2/imageBulletinActivity.java
@@ -1,5 +1,8 @@
 package com.androidtest.HttpParser2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,6 +16,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.webkit.WebView;
 import android.widget.Toast;
@@ -27,49 +31,64 @@ public class imageBulletinActivity extends Activity {
 	String address_replace_skin = "http://clien.career.co.kr/cs2/skin";
 	String address_replace_data = "http://clien.career.co.kr/cs2/data";
 	
-	String headtag ="<html xmlns=\"http://www.w3.org/1999/xhtml\">"+
-	"<head>"+
-	"<link href=\"http://clien.career.co.kr/cs2/css/style.css?v=20110712\" rel=\"stylesheet\" type=\"text/css\" />"+
-	"<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"/>"+ 
-	"<meta http-equiv=\"Imagetoolbar\" content=\"no\" />"+
-	"<meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width\" />"+
-	"<link rel=\"stylesheet\" href=\"http://clien.career.co.kr/cs2/style.css?v=20110712\" type=\"text/css\" />"+	
-	"</head> <html><body topmargin=\"0\" leftmargin=\"0\">"+
-	"<style>"+ 
-	".resContents      img { max-width:280; width: expression(this.width > 280 ? 280: true); }"+
-	".commentContents  img { max-width:280; width: expression(this.width > 280 ? 280: true); }"+
-	"</style>"+	
-	"<style type=\"text/css\">"+
-	".board_main{clear:both;padding-top:5px;}"+
-	".view_content{clear:both;font:12px \"굴림\",Gulim;line-height:19px;padding:0px 4px 0px 5px;word-break:break-all;}"+
-	".view_content_btn{text-align:right;margin-bottom:10px;}"+
-	".view_content_btn img{display:inline !important;}"+
-	".view_content_btn2{margin:0 auto;width:300px;overflow:hidden;}"+
-	".view_content_btn3{margin:0 auto;width:300px;}"+
-	".view_content_btn3 li{float:left;padding-right:6px;}"+
-	".view_content_btn3 img{display:inline !important;}"+
-	".view_content_btn2 li{float:left;padding-right:6px;}"+
-	".ad_area1{clear:both;text-align:center;padding:20px 0 20px 0;font-weight:normal;}"+
-	".reply_head{background-color:#f3f3f3;height:16px;padding:4px 14px 4px 9px;line-height:16px;margin-top:20px;}"+
-	".repla_head a{color:#374273 !important;text-decoration:none !important;}"+
-	".reply_head1{background-color:#f3f3f3;height:16px;padding:4px 14px 4px 9px;line-height:16px;margin-top:20px;}"+
-	".reply_info{float:left;color:#898989;font-size:11px;}"+
-	".reply_info .user_id{color:#374273;padding-right:2px;}"+
-	".reply_info .block{font-size:12px;padding:0 5px 0 13px;_line-height:19px;}"+
-	".reply_head li{float:left;}"+
-	".reply_btn{float:right;}"+
-	".reply_btn li{padding-left:3px;}"+
-	".reply_btn img{vertical-align:top;padding-top:1px;}"+
-	".reply_btn .report{padding:3px 0 0 7px;}"+
-	".reply_btn .ip{color:#b2b2b2;padding-right:7px;font-size:12px;font-weight:normal;}"+
-	".reply_content{clear:both;word-break:break-all;padding:4px 4px 0px 9px;margin-bottom:0px;font:12px \"굴림\",Gulim,AppleGothic;line-height:19px;color:#000;font-weight:normal;}"+
-	".ccl{width:280px;text-align:right;margin:0 auto;}"+
-	".ccl img{display:inline !important;}"+
-	".signature{color:#667e99;margin-top:20px;}"+
-	".signature img{display:inline !important;}"+
-	".signature dl{width:280px;overflow:hidden;margin:0 auto;}"+
-	".signature dt{border-bottom:2px solid #c8d0d9;height:18px;overflow:hidden;}"+
-	".signature dd{padding:7px 0 0 20px;}"+
+	String headtag ="<html xmlns=\"http://www.w3.org/1999/xhtml\">"+"\n"+
+	"<head>"+"\n"+
+	"<link href=\"http://clien.career.co.kr/cs2/css/style.css?v=20110712\" rel=\"stylesheet\" type=\"text/css\" />"+"\n"+
+	"<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"/>"+ "\n"+
+	"<meta http-equiv=\"Imagetoolbar\" content=\"no\" />"+"\n"+
+	"<meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, width=device-width\" />"+"\n"+
+	"<link rel=\"stylesheet\" href=\"http://clien.career.co.kr/cs2/style.css?v=20110712\" type=\"text/css\" />"+"\n"+	
+	"</head> <html><body topmargin=\"0\" leftmargin=\"0\">"+"\n"+
+	"<style>"+"\n"+ 
+	".resContents      img { max-width:280; width: expression(this.width > 280 ? 280: true); }"+"\n"+
+	".commentContents  img { max-width:280; width: expression(this.width > 280 ? 280: true); }"+"\n"+
+	"</style>"+"\n"+	
+	"<style type=\"text/css\">"+"\n"+
+	".board_main{clear:both;padding-top:5px;width:280px;}"+"\n"+
+	//".board_main table{width:100%;text-align:center;border-bottom:2px solid #ebebeb;}"+"\n"+
+	//".board_main th{background-color:#dde1e6;border-bottom:2px solid #fff;padding:8px 0 6px 0;*padding:8px 0 3px 0;}"+"\n"+
+	//".twl{border-left:1px solid #fff;border-right:1px solid #fff;}"+"\n"+
+	//".board_main td{font:11px \"굴림\",Gulim,AppleGothic;color:#374273;border-bottom:1px solid #ebebeb;padding:4px 0 3px 0;}"+"\n"+
+	//".board_main img{vertical-align:top;}"+"\n"+
+	".view_head{background-color:#dde1e6;height:17px;color:#374273;padding:6px 0 2px 0;*padding:4px 0 3px 0;width:300px;}"+"\n"+
+	".view_head a{color:#374273 !important;text-decoration:none !important;}"+"\n"+
+	".user_info{float:left;padding-left:8px;}"+"\n"+
+	".user_info span{font-size:11px;color:#808080;}"+"\n"+
+	".post_info{float:right;padding-right:13px;}"+"\n"+
+	".view_title{clear:both;border-bottom:5px solid #dde1e6;padding:10px 0 4px 18px;float:left;width:300px;word-break:break-all;}"+"\n"+
+	".view_title div{float:left;width:280px;}"+"\n"+
+	".title_btn{float:right;padding:20px 5px 0 0;}"+"\n"+
+	".view_title h3{font-size:12px;font-weight:normal;}"+"\n"+
+	".view_title h4{font:14px \"굴림\",Gulim,AppleGothic;font-weight:bold;}"+"\n"+
+	".view_content{clear:both;font:12px \"굴림\",Gulim;line-height:19px;padding:0px 4px 0px 5px;word-break:break-all;}"+"\n"+
+	".view_content_btn{text-align:right;margin-bottom:10px;}"+"\n"+
+	".view_content_btn img{display:inline !important;}"+"\n"+
+	".view_content_btn2{margin:0 auto;width:100px;overflow:hidden;}"+"\n"+
+	".view_content_btn3{margin:0 auto;width:100px;}"+"\n"+
+	".view_content_btn3 li{float:left;padding-right:6px;}"+"\n"+
+	".view_content_btn3 img{display:inline !important;}"+"\n"+
+	".view_content_btn2 li{float:left;padding-right:6px;}"+"\n"+
+	".ad_area1{clear:both;text-align:center;padding:20px 0 20px 0;font-weight:normal;}"+"\n"+
+	".reply_head{background-color:#f3f3f3;height:16px;padding:4px 14px 4px 9px;line-height:16px;margin-top:20px;width:280px;}"+"\n"+
+	".repla_head a{color:#374273 !important;text-decoration:none !important;}"+"\n"+
+	".reply_head1{background-color:#f3f3f3;height:16px;padding:4px 14px 4px 9px;line-height:16px;margin-top:20px;width:200px;}"+"\n"+
+	".reply_info{float:left;color:#898989;font-size:11px;}"+"\n"+
+	".reply_info .user_id{color:#374273;padding-right:2px;}"+"\n"+
+	".reply_info .block{font-size:12px;padding:0 5px 0 13px;_line-height:19px;}"+"\n"+
+	".reply_head li{float:left;}"+"\n"+
+	".reply_btn{float:right;}"+"\n"+
+	".reply_btn li{padding-left:3px;}"+"\n"+
+	".reply_btn img{vertical-align:top;padding-top:1px;}"+"\n"+
+	".reply_btn .report{padding:3px 0 0 7px;}"+"\n"+
+	".reply_btn .ip{color:#b2b2b2;padding-right:7px;font-size:12px;font-weight:normal;}"+"\n"+
+	".reply_content{clear:both;word-break:break-all;padding:4px 4px 0px 9px;margin-bottom:0px;font:12px \"굴림\",Gulim,AppleGothic;line-height:19px;color:#000;font-weight:normal;}"+"\n"+
+	".ccl{width:280px;text-align:right;margin:0 auto;}"+"\n"+
+	".ccl img{display:inline !important;}"+"\n"+
+	".signature{color:#667e99;margin-top:20px;}"+"\n"+
+	".signature img{display:inline !important;}"+"\n"+
+	".signature dl{width:280px;overflow:hidden;margin:0 auto;}"+"\n"+
+	".signature dt{border-bottom:2px solid #c8d0d9;height:18px;overflow:hidden;}"+"\n"+
+	".signature dd{padding:7px 0 0 20px;}"+"\n"+
 	"</style>";
 	
 	String endtag = " </body></html>";
@@ -90,8 +109,8 @@ public class imageBulletinActivity extends Activity {
         
         webView = (WebView)this.findViewById(R.id.imgbulletinwebview);
         webView.getSettings().setPluginsEnabled(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.setInitialScale(1);
+        //webView.getSettings().setUseWideViewPort(true);
+        //webView.setInitialScale(1);
         
         intent = getIntent();
         originalLink = intent.getExtras().getString("Link").toString();
@@ -108,7 +127,7 @@ public class imageBulletinActivity extends Activity {
 		new parseHtml().execute();
 	}
 	
-	private int buildTagList2() throws MalformedURLException,IOException {
+	private int buildTagList() throws MalformedURLException,IOException {
 		String content_str = "";
 		String reply_str = "";
 	
@@ -119,53 +138,27 @@ public class imageBulletinActivity extends Activity {
 		source.fullSequentialParse();
 		
 		//contents
-		List<Element> contenttags = source.getAllElementsByClass("view_content");
+		List<Element> boardmaintags = source.getAllElementsByClass("board_main");
 		
-		for (int i = 0; i < contenttags.size(); i++) {
-
-			Element contentElement = (Element) contenttags.get(i);
-			String cStr = contentElement.toString();
-
-			if(cStr.contains("<object"))
-			{
-				cStr = cStr.replaceAll("height=\"[0-9]*\"","height=\"150\"");
-				cStr = cStr.replaceAll("width=\"[0-9]*\"","width=\"200\"");
-			}
+		for(int z = 0; z < boardmaintags.size(); z++)
+		{
+			Element boardmainElement = (Element) boardmaintags.get(z);
+			String cStr = boardmainElement.toString();
+			
 			content_str += cStr;
 			
 			result = 1;
 		}
 		
-		//contents - reply
-		List<Element> replytags = source.getAllElementsByClass("reply_base");
-		
-		for (int i = 0; i < replytags.size(); i++) {
-
-			Element replyElement = (Element) replytags.get(i);
-			String cStr = replyElement.toString();
-			//Log.d("replytags",i + " : cStr : "+cStr);
-			reply_str += cStr;
+		if(content_str != null)
+		{
+			content_str = content_str.replaceAll("Hit : [0-9]* ,","");
+			content_str = content_str.replaceAll("Vote : [0-9]*","");
+			content_str = content_str.replaceAll("%","%25");
+			content_str = content_str.replaceAll("\\.\\./skin",address_replace_skin);
+			content_str = content_str.replaceAll("\\.\\./data",address_replace_data);
 		}
 		
-		content_str = content_str.replaceAll("<div class=\"signature\"","<!-- <div class=\"signature\"");
-		content_str = content_str.replaceAll("</dd></dl></div>","</dd></dl></div> -->");
-		//content_str = content_str.replaceAll("<div class=\"ccl\"","<!-- <div class=\"ccl\"");
-		//content_str = content_str.replaceAll("<div class=\"signature\"><dl><dt>(.*?)</dd></dl></div>","aaaa");
-		
-		content_str = content_str.replaceAll("<div class=\"ExifInfo\" style=\"width:[0-9]*px;\">"
-				,"<div class=\"ExifInfo\" style=\"width:280px;\">");
-		content_str = content_str.replaceAll("%","%25");
-		content_str = content_str.replaceAll("\\.\\./skin",address_replace_skin);
-		content_str = content_str.replaceAll("\\.\\./data",address_replace_data);
-		//Log.d("content_str",content_str);
-		
-		reply_str = reply_str.replaceAll("%","%25");
-		reply_str = reply_str.replaceAll("\\.\\./skin",address_replace_skin);
-		reply_str = reply_str.replaceAll("\\.\\./data",address_replace_data);
-		reply_str = reply_str.replaceAll("</textarea>(.*?)</div>","</textarea></div>");
-		//Log.d("replytags",reply_str);
-		content_str += reply_str;		
-				
 		imageBulletinItem ci  = new imageBulletinItem(content_str);
 		
 		setimageBulletinItem(ci);
@@ -196,7 +189,7 @@ public class imageBulletinActivity extends Activity {
 		}
 		
 		//-------------------------------------------------------------------------------------------------------//
-		/*
+		///*
 		FileOutputStream fileout = null;
 		try {
 			fileout = new FileOutputStream(new File(
@@ -241,7 +234,7 @@ public class imageBulletinActivity extends Activity {
 			while(result == 0)
 			{
 				try {
-					result = buildTagList2();
+					result = buildTagList();
 					
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
